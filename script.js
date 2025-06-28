fcb34138cca54eafff600f7e020645c9d7cbe15f
Alenny M Almonte <almontealenny@gmail.com>
2025-06-28 11:08:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function appendValue(value) {
+function append(value) {
   document.getElementById("display").value += value;
 }
 
 function clearDisplay() {
-  document.getElementById("display").value = '';
+  document.getElementById("display").value = "";
+}
+
+function backspace() {
+  let current = document.getElementById("display").value;
+  document.getElementById("display").value = current.slice(0, -1);
 }
 
 function calculate() {
   try {
     let result = eval(document.getElementById("display").value);
     document.getElementById("display").value = result;
-  } catch (e) {
+  } catch {
     document.getElementById("display").value = "Error";
   }
 }
